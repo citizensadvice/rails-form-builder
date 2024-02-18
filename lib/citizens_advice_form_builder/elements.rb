7de41b020a7ec03de8fa8b8366ba857ffcffe5f6
Leeky <robert.lee-cann@citizensadvice.org.uk>
2024-02-18 11:52:13 +0000

    Extract individual form methods
    Moving the form methods into separate classes will make it much easier
    to share functionality between them.

---
+++ lib/citizens_advice_form_builder/elements.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require_relative "elements/base"
+require_relative "elements/text_input"
---
+++ lib/citizens_advice_form_builder/elements/text_input.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module CitizensAdviceFormBuilder
+  module Elements
+    class TextInput < Base
+      def render
+        component = CitizensAdviceComponents::TextInput.new(
+          name: field_id,
+          label: label,
+          type: :text,
+          options: {
+            hint: hint,
+            optional: optional,
+            value: current_value,
+            error_message: error_message,
+            additional_attributes: { name: field_name }
+          }
+        )
+
+        component.render_in(@template)
+      end
+    end
+  end
+end
