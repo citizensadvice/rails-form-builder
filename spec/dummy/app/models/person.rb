66c6560c316e93b821329c1e2174ad6622a13934
Leeky <robert.lee-cann@citizensadvice.org.uk>
2024-02-23 16:49:33 +0000

    Add 'cads_collection_check_boxes' method
    NB: Because the design system component 'CheckboxGroup' doesn't
    currently allow the HTML 'name' attribute to be customised in a way
    that's compatible with Rails' naming scheme, I've had to recreate the
    generated HTML from scratch.

--- spec/dummy/app/models/person.rb
+++ spec/dummy/app/models/person.rb
@@ -7,9 +7,10 @@ class Person
   include ActiveModel::Attributes
   include ActiveRecord::AttributeAssignment
 
-  attr_accessor :first_name, :last_name, :address, :date_with_hint, :required_date, :favourite_drink
+  attr_accessor :first_name, :last_name, :address, :date_with_hint, :required_date, :favourite_drink, :pizza_toppings
 
   attribute :date_of_birth, :date
+  attribute :pizza_toppings, array: true
 
   validates :first_name, presence: true
 end
