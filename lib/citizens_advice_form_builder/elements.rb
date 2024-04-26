1fcb26a2b31a6f3297efd7331632f222e792fe12
Leeky <robert.lee-cann@citizensadvice.org.uk>
2024-04-26 15:37:18 +0100

    Adding `cads_error_summary`
    This uses the design system's `ErrorSummary` component, with a simple
    mapping between the auto-generated HTML ids and validation error
    messages from ActiveModel and the href/message format that the component
    expects.

--- lib/citizens_advice_form_builder/elements.rb
+++ lib/citizens_advice_form_builder/elements.rb
@@ -5,6 +5,7 @@ require_relative "elements/text_input"
 require_relative "elements/button"
 require_relative "elements/text_area"
 require_relative "elements/date_input"
+require_relative "elements/error_summary"
 
 require_relative "elements/collections/radio_buttons"
 require_relative "elements/collections/check_boxes"
