050d24b22afa3744980fd194c3786f0cc34d1a3f
Leeky <robert.lee-cann@citizensadvice.org.uk>
2024-04-30 13:54:31 +0100

    Allow icons to be added to buttons
    Add new `icon_left` and `icon_right` options to the `cads_button`
    method. This allows you to add an icon to the left or right of the
    button text.
    
    Icons can be any of those specified in the design system:
    https://citizens-advice-design-system.netlify.app/foundations/icons/

--- lib/citizens_advice_form_builder/elements/button.rb
+++ lib/citizens_advice_form_builder/elements/button.rb
@@ -3,15 +3,29 @@
 module CitizensAdviceFormBuilder
   module Elements
     class Button < Base
-      def initialize(template, object, button_text: nil, **kwargs)
+      def initialize(template, object, button_text: nil, icon_left: nil, icon_right: nil, **kwargs)
         super(template, object, nil, **kwargs)
 
         @button_text = button_text
+        @icon_left = icon_left
+        @icon_right = icon_right
       end
 
       def render
         component = CitizensAdviceComponents::Button.new(**options)
         component.with_content(@button_text)
+
+        if @icon_left
+          component.with_icon_left do
+            icon_left_class.new.render_in(@template)
+          end
+        end
+
+        if @icon_right
+          component.with_icon_right do
+            icon_right_class.new.render_in(@template)
+          end
+        end
 
         component.render_in(@template)
       end
@@ -20,6 +34,14 @@ module CitizensAdviceFormBuilder
 
       def default_options
         { type: :submit, variant: :primary }
+      end
+
+      def icon_left_class
+        "CitizensAdviceComponents::Icons::#{@icon_left.to_s.camelize}".constantize
+      end
+
+      def icon_right_class
+        "CitizensAdviceComponents::Icons::#{@icon_right.to_s.camelize}".constantize
       end
     end
   end
